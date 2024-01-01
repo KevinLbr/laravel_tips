4ecf78c8f4d8d2de93ef6b2092a3871a85227f14
Kevin Labre <30754771+KevinLbr@users.noreply.github.com>
2024-01-01 23:48:09 +0100

    Update Model_with_img.php

--- Model_with_img.php
+++ Model_with_img.php
@@ -2,38 +2,34 @@
 
 namespace App\Models;
 
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
-use Backpack\CRUD\CrudTrait;
 
 class Product extends Model
 {
-    use CrudTrait;
-
     protected $table = 'products';
-    protected $fillable = ["name", "description", "image", "category_id", "price"];
+    
+    protected $guarded = [
+        "id"
+    ];
 
     private $product_folder = "product_folder";
 
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function getImageAttribute()
+    public function getImageAttribute(): string
     {
         return \Storage::disk('public')->url("{$this->product_folder}/{$this->attributes['image']}");
     }
 
-    public function setImageAttribute($value)
+    public function setImageAttribute($value): void
     {
-        if (starts_with($value, 'data:image')) {
+        if (starts_with($value, 'data:image')) 
+        {
             $image               = \Image::make($value)->encode('png')->resize(100,100);
             $filename            = "image". str_random(5).".png";
             $annee               = date('Y');
             $mois                = date('m');
             $destination_path    = "{$annee}/{$mois}";
-            if (\Storage::disk('public')->exists("{$this->product_folder}/{$destination_path}/{$filename}")) {
+            if (\Storage::disk('public')->exists("{$this->product_folder}/{$destination_path}/{$filename}")) 
+            {
                 \Storage::disk('public')->delete("{$this->product_folder}/{$destination_path}/{$filename}");
             }
             \Storage::disk('public')->put("{$this->product_folder}/{$destination_path}/{$filename}", $image->stream());
